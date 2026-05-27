90cf834947a349590e1b4081f7714a1acb74f30f
teteumachado <trintascripter@hotmail.com>
2026-05-27 00:05:16 -0300

    feat: config package

--- apps/api/source/index.ts
+++ apps/api/source/index.ts
@@ -1,8 +1,9 @@
 import { Elysia } from 'elysia'
 import { node } from '@elysia/node'
+import { env } from '@workspace/config/env'
 
 const app = new Elysia({ adapter: node() })
-  .get('/', () => 'Hello Elysia!')
+  .get('/', () => env.DATABASE_URL)
   .listen(8000, ({ hostname, port }) => {
     console.log(`🦊 Elysia is running at ${hostname}:${port}`)
   })
