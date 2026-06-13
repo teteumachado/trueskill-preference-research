861f36f4765e5cac891a3ad3457c8e6fd2aa444f
teteumachado <trintascripter@hotmail.com>
2026-06-13 01:22:05 -0300

    feat: add auth middleware to protect dashboard route

---
+++ apps/web/proxy.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import { headers } from "next/headers";
+import { auth } from "@workspace/auth/server";
+
+export async function proxy(request: NextRequest) {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard"], // Specify the routes the middleware applies to
+};
