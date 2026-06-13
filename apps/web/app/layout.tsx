7093f4db6f2d9cc82b7ca1de8a48d1365a292ae4
teteumachado <trintascripter@hotmail.com>
2026-06-13 01:21:58 -0300

    feat: add sidebar UI components

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,9 +1,10 @@
 import { Geist, Geist_Mono } from 'next/font/google'
 
 import '@workspace/ui/globals.css'
-import { ThemeProvider } from '@/components/theme-provider'
 import { cn } from '@workspace/ui/lib/utils'
+import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from '@workspace/ui/components/sonner'
+import { TooltipProvider } from '@workspace/ui/components/tooltip'
 
 const geist = Geist({ subsets: ['latin'], variable: '--font-sans' })
 
@@ -24,10 +25,12 @@ export default function RootLayout({
       className={cn('antialiased', fontMono.variable, 'font-sans', geist.variable)}
     >
       <body>
-        <ThemeProvider>
-          {children}
-          <Toaster />
-        </ThemeProvider>
+        <TooltipProvider>
+          <ThemeProvider>
+            {children}
+            <Toaster />
+          </ThemeProvider>
+        </TooltipProvider>
       </body>
     </html>
   )
